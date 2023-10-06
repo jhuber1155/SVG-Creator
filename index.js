7b6a13e8ba43fc77fffbf5c7ad616bb4df949a3d
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-10-05 19:40:20 -0700

    Got the write file partially working

--- index.js
+++ index.js
@@ -2,28 +2,30 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What letters would you like to use? Please choose up to 3 letters',
-            name: 'letter',
-        },
-        {
-            type: 'input',
-            message: 'Please choose what color you would like to make the text',
-            name: 'letterColor',
-        },
-        {
-            type: 'list',
-            message: 'Please choose what shape you would like to use: Circle, Triangle, or square',
-            name: 'shape',
-        },
-        {
-            type: 'input',
-            message: 'Please choose what color you would like to make the shape',
-            name: 'shapeColor',
-        },
-    ])
+  .prompt([
+    {
+      type: 'input',
+      message: 'What letters would you like to use? You may choose up to 3 letters',
+      name: 'letters',
+    },
+    {
+      type: 'input',
+      message: 'What color would you like to make the letters?',
+      name: 'letterColor',
+    },
+    {
+      type: 'list',
+      message: 'What shape would you like to have behind the letters? A triangle, circle, or square?',
+      choices: ['Triangle', 'Circle', 'Square'],
+      name: 'shape',
+    },
+    {
+        type: 'input',
+        message: 'What color would you like to make the background shape?',
+        name: 'shapeColor',
+    },
+  ])
     .then((data) =>{
-            fs.writeFile('logo.svg', data, (err) => err ? console.error(err) : console.log('Generated logo.svg'))
-        })
+        fs.writeFile("logo.svg", JSON.stringify(data), (err) => {
+            err ? console.error(err) : console.log("Generated logo.svg")})
+        });
